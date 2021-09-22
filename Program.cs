3433a31ea961d0ead4da82b5d7cef33938b7d6dd
seannleckie <seannleckie@gmail.com>
2021-09-22 15:03:37 -0400

    Minor: make GatasVersion readonly

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace GrandstreamATAConfigurator
     {
         // BEGIN GLOBAL VARIABLES
 
-        private static Version GatasVersion = new("2.3.0");
+        private static readonly Version GatasVersion = new("2.3.0");
 
         // for locating, connecting to ATA
         private static NetworkInterface _interfaceToUse;
